0ea62fb2236667a38b6507d0d546cadf132f19d6
ashcbrd <cabardoash@gmail.com>
2023-07-06 21:53:45 +0800

    initial commit

--- .history/src/app/@authModal/(.)sign-in/page_20230706210211.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-interface pageProps {}
-
-const page: FC<pageProps> = ({}) => {
-  return <div className="w-screen h-screen m-auto">INTERCEPT</div>;
-};
-
-export default page;
--- .history/src/components/CloseModal_20230706213553.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-const CloseModal: FC = () => {
-  return <div>CloseModal</div>;
-};
-
-export default CloseModal;
