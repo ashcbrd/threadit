08821c8c71b02fe08a15b948dde3339d793b4225
ashcbrd <cabardoash@gmail.com>
2023-07-19 06:31:18 +0800

    fix: removed signup component

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -10,7 +10,7 @@ const SignIn: FC = () => {
       <div className="flex flex-col space-y-2 text-center">
         {/* <Icons.logo className="mx-auto h-6 w-6" /> */}
         <p>LOGO</p>
-        <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome!</h1>
         <p className="text-sm max-w-xs mx-auto">
           By continuing, you are setting up a Threadit account and agree to our
           User Agreement and Privacy Policy
@@ -18,10 +18,6 @@ const SignIn: FC = () => {
 
         {/* sign in form */}
         <UserAuthForm />
-
-        <p className="px-8 text-center text-sm text-zinc-700">
-          New to Threadit?{" "}
-        </p>
       </div>
     </div>
   );
